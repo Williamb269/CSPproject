576b770855d5bd904922d64682af444674f8bce4
BT_1N3_05 <williamb269@nycstudents.net>
2024-10-31 13:37:17 -0400

    Update

---
+++ src/LinearEquation.java
@@ -0,0 +1,27 @@
+public class LinearEquation {
+    private int x1, y1, x2, y2;
+    private double slope;
+    private double yIntercept;
+    private double distance;
+
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    double distance() {
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
+    double slope() {
+            return (double) (y2 - y1) / (x2 - x1);
+    }
+    double yIntercept() {
+        return y1 - slope * x1;
+    }
+    String equation() {
+
+        return ("y = " + slope() + "x + " + yIntercept());
+    }
+
+}
